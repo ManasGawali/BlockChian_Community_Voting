8de3548b1a7c6b241967bb35792eb415341bb6b1
ManasGawali <manasmgawali@gmail.com>
2025-04-23 18:46:16 +0530

    name changes

--- blockchain/contracts/ElectionFactory.sol
+++ blockchain/contracts/ElectionFactory.sol
@@ -17,7 +17,6 @@ contract ElectionFactory {
         require(voters.length > 0, "At least one voter required");
         require(candidates.length >= 2, "At least two candidates required");
         require(voters.length <= 1000, "Too many voters");
-        require(adminToElection[msg.sender] == address(0), "You have already created a community");
         require(msg.value >= 0.01 ether * voters.length, "Not enough ETH to cover gas fees");
 
         Election newElection = (new Election){value: msg.value}(msg.sender, name, candidates, voters);
